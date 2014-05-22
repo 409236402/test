032cce8c9f42aa7d06b601d34067378e4116eaf1
zhaoyangmei <409236402@qq.com>
2014-05-22 17:16:38 +0800

    加入js

--- professional_business/js/main.js
+++ professional_business/js/main.js
@@ -3,8 +3,37 @@
  */
 
 $(function(){
-
+    app.imgSwap();
 });
 
 var ui={},
     app = {};
+
+app.imgSwap = function(){
+    var dots = $(".img-swap-dot-a");
+    var imgs =$(".img-swap img");
+    var i = 0,
+        timer = 1000;
+    imgs.fadeOut(0).eq(0).fadeIn(0);//隐藏所有图片，第一张淡入
+    var clock = setInterval(function(){
+        if(imgs.length > (i+1)){
+            imgs.eq(i).fadeOut(timer).next("img").fadeIn(timer);
+            i++;
+        }else{
+            imgs.eq(i).fadeOut(timer).siblings("img").eq(0).fadeIn(timer);
+            i=0;
+        }
+        dots.removeClass("img-swap-dot-a-active");
+        dots.eq(i).addClass("img-swap-dot-a-active");
+    },4000);
+
+    dots.click(function(){
+        if($(this).hasClass("img-swap-dot-a-active")){
+            return;
+        }
+        dots.removeClass("img-swap-dot-a-active");
+        $(this).addClass("img-swap-dot-a-active");
+        i=$(this).index();
+        imgs.fadeOut(timer).eq(i).fadeIn(timer);
+    });
+};
